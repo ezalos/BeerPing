8f5ba52a805700d3f49caaa434652a61baee85cb
kipit <kipit@42l.fr>
2020-11-23 17:06:26 +0100

    setsock opt

--- includes/head.h
+++ includes/head.h
@@ -6,7 +6,7 @@
 /*   By: ldevelle <ldevelle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 11:15:02 by ldevelle          #+#    #+#             */
-/*   Updated: 2020/11/18 18:01:16 by amartinod        ###   ########.fr       */
+/*   Updated: 2020/11/23 16:40:16 by amartinod        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,16 @@
 
 # include "prototypes_ping.h"
 
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <string.h>
+#include <stdio.h> // printf family
+#include <sys/types.h> // getaddrinfo
+#include <sys/socket.h> //socket, setsockopt and getaddrinfo
+#include <netdb.h> // getaddrinfo
+#include <string.h> // memset pas autorisé mais chaud de laisser. #libft_free
 #include <unistd.h>
-#include <arpa/inet.h>
+#include <arpa/inet.h> //inet_ntop and inet_pton
+#include <sys/time.h> // get time of the day and timeval struct
+
+typedef FAILURE		-1
 
 void	print_ip(struct addrinfo *p);
 
--- srcs/main.c
+++ srcs/main.c
@@ -9,8 +9,13 @@ typedef struct			s_infos
 
 /*
 **	From a string containing an URL, get us the struct addrinfo
+** FOR REVERSE DNS
+** Historically, passing a host's own hostname to getaddrinfo() has been a popular
+** technique for determining that host's IP address(es), but this is fragile,
+** and doesn't work reliably in all cases. The appropriate way for software to
+** discover the IP address(es) of the host it is running on is to use getifaddrs(3).
 */
-struct addrinfo		*get_addr_info_from_url(const char *url)
+struct addrinfo		*dns_lookup(const char *url)
 {
 	struct addrinfo   hints;
     struct addrinfo   *res;
@@ -18,6 +23,7 @@ struct addrinfo		*get_addr_info_from_url(const char *url)
 
     res = NULL;
     memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
     error = getaddrinfo(url, NULL, &hints, &res);
     if (error)
     {
@@ -29,18 +35,58 @@ struct addrinfo		*get_addr_info_from_url(const char *url)
 
 
 /*
-**	This function takes care of creating a ready to use socket for communication
-**	with the struct addrinfo
-**	setsockopt may be need to be called
-*/
-// Does socket takes care of internet communication ?
-int	    open_socket_for_communication_with_server(struct addrinfo *addr)
+ * man icmp
+ * Outgoing packets automatically have an IP header prepended to them
+ * (based on the destination address).
+ * Incoming packets are received with the IP header and options intact.
+ * ICMP sockets can be opened with the SOCK_DGRAM socket type without requiring
+ * root privileges. Datagram oriented ICMP sockets offer a subset of the
+ * functionality available to raw ICMP sockets.
+ *
+ * man setsockopt
+ * To manipulate options at the socket level, level is specified as SOL_SOCKET.
+ * To manipulate options at any other level the protocol number of the appropriate
+ * protocol controlling the option is supplied. See man ip for the protocol number
+ *
+ * Option at IP level (man ip)
+ * There are several IP-level setsockopt(2) /getsockopt(2) options.
+ * IP_TTL may be used to set the time-to-live fields in the IP header for SOCK_DGRAM
+ *
+ * int ttl = 60; (max = 255)
+ * setsockopt(s, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl));
+ *
+ * Option at socket level (man setsockopt)
+ * SO_RCVTIMEO is an option to set a timeout value for input operations.
+ * It accepts a struct timeval parameter with the number of seconds.
+*/ 
+// Gives the timeout delay for receiving packets 
+// in seconds 
+#define RECV_TIMEOUT 1 
+int	    open_socket_and_set_options(void)
 {
-    int sock;
+    int             sock_fd;
+    int             ret;
+    int             ttl;
+    struct timeval  tv_input;
 
-    sock = socket(addr->ai_family, SOCK_RAW, addr->ai_protocol);
-    printf("sock return : %d\n", sock);
-    return 0;
+    ttl = 64;
+    tv_input.tv_sec = RECV_TIMEOUT;
+    tv_input.tv_usec = 0;
+    sock_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP);
+    if (sock_fd < 0)
+    {
+        printf("The opening of the socket failed\n");
+        return (sock_fd);
+    }
+    ret = setsockopt(sock_fd, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl));
+    if (ret != FAILURE)
+        ret = setsockopt(sock_fd, SOL_SOCKET, &tv_input, sizeof(tv_input));
+    if (ret == FAILURE)
+    {
+        printf("Failed to set socket options\n");
+        return (FAILURE); // should I close the socket ?
+    }
+    return (sock_fd);
 }
 
 
@@ -63,14 +109,19 @@ void				*listen_for_answers(void);
 
 int		main(int ac, char **av)
 {
-    (void)ac;
-	(void)av;
 	t_infos		info;
+    int         sock_fd;
 	char 		ip_str[INET6_ADDRSTRLEN];
 
-	info.addr = get_addr_info_from_url("www.google.com");
-    inet_ntop(info.addr->ai_family, info.addr, ip_str, sizeof(ip_str));
-    open_socket_for_communication_with_server(info.addr);
-    print_ip(info.addr);
+    if (ac > 1)
+    {
+        info.addr = dns_lookup(av[1]);
+        inet_ntop(info.addr->ai_family, info.addr, ip_str, sizeof(ip_str));
+        sock_fd = open_socket_and_set_options();
+        printf("sock return : %d\n", sock_fd);
+        print_ip(info.addr);
+    }
+    else
+        printf("Please enter an url\n");
 	return (0);
 }
